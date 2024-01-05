9dc7813e1da18c11af03e6826e8e7c83835256aa
mdtalha109 <mdtalha1041@gmail.com>
2024-01-05 23:41:51 +0530

    style: added separate style file

--- src/components/CategoriesProduct/CategoriesProduct.js
+++ src/components/CategoriesProduct/CategoriesProduct.js
@@ -1,3 +1,4 @@
+import { Container } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { remote_config } from '../../config/remoteURL';
@@ -5,7 +6,13 @@ import { remote_config } from '../../config/remoteURL';
 import Product from '../Product';
 import ProductSkeletonLoader from '../SkeletonLoader/productSkeletonLoader';
 
-const CategoriesProduct = ({categoryName, Limit}) => {
+import './CategoriesProduct.css'
+
+const CategoriesProduct = ({categoryName, Limit, heading, style}) => {
+
+    const CategoriesProductStyle= {
+        ...style
+    }
 
     const [product, setProduct] = useState([])
     const [loading, setLoading] = useState([])
@@ -20,13 +27,18 @@ const CategoriesProduct = ({categoryName, Limit}) => {
    
   return (
       <>
-       
+      <div style={CategoriesProductStyle}>
+      <h2>{heading}</h2>
+      
         {<div className="container-product_card">
             {loading ? <div style={{width:"100%", display: "flex", justifyContent:"center"}}><ProductSkeletonLoader/></div> : ""}
             {product && product.map(product => <Product product = {product}/>)}
             
             </div>  
         }
+        </div>
+      
+      
       </>
   )
 }
