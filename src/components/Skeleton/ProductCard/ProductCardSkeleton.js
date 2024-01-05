eda277d2b2caa2ba4fe41e1d2a4c3aba471d86d6
mdtalha109 <mdtalha1041@gmail.com>
2024-01-05 23:58:15 +0530

    style: fixed style in skeleton

--- src/components/Skeleton/ProductCard/ProductCardSkeleton.js
+++ src/components/Skeleton/ProductCard/ProductCardSkeleton.js
@@ -5,9 +5,9 @@ const ProductCardSkeleton = () => {
   return (
     <>
     
-        <div className='cards'>
-            <Skeleton variant="rectangular" width={140} height={118} style={{borderRadius:'6%', margin: '5px'}} />
-            <Skeleton variant="text" width={100} height={40} />
+        <div className="cards" style={{display: "flex", flexDirection: "column", width: "100%" }}>
+            <Skeleton variant="rectangular" height={118} style={{borderRadius:'6%', margin: '5px', width:"90%" }} />
+            <Skeleton variant="text" width={100} height={40} style={{ width:"50%" }} />
             <Skeleton variant="text" width={100} height={20} />
         </div>
     
