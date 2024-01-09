a8f465f21b251a365135d8957202d1078dffcb20
mdtalha109 <mdtalha1041@gmail.com>
2024-01-09 23:40:49 +0530

    feat: added order list
    listed the all previous order done by user

--- src/Pages/ProfilePage/ProfilePage.js
+++ src/Pages/ProfilePage/ProfilePage.js
@@ -1,17 +1,21 @@
 import React, {useState, useEffect} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
-import {Form, Button, Row, Col} from 'react-bootstrap'
+import {Link, NavLink, useNavigate} from 'react-router-dom'
+import {Form, Button, Row, Col, Container} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
 
 import { toast, ToastContainer } from "react-toastify"
 import {getUserDetails, updateUserProfile} from '../../actions/userActions'
-// import '..SignupScreen/SignupScreen.css'
+import Input from '../../components/ui/Input'
+import styles from './index.module.css'
+import { remote_config } from '../../config/remoteURL'
+import axios from 'axios'
 
 export const ProfileScreen = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
+    const [allOrder, setAllOrder] = useState('');
 
     const dispatch = useDispatch()
 
@@ -22,9 +26,6 @@ export const ProfileScreen = () => {
     const userLogin = useSelector(state => state.userLogin)
     const {userInfo} = userLogin
 
-    
-    
-    
 
     const navigate = useNavigate()
 
@@ -46,74 +47,120 @@ export const ProfileScreen = () => {
     },[dispatch, userInfo])
 
     const submitHandler = () => {
-
-        //dispatch updateprofile 
-         dispatch(updateUserProfile({id : userInfo._id, name, email, password}))
-      
-       
+         dispatch(updateUserProfile({id : userInfo._id, name, email, password}))  
     }
+
+    useEffect(() => {
+        const fetchAllOrder = async () => {
+            try {
+                const config = {
+                    headers: {
+                        Authorization: `Bearer ${userInfo.token}`,
+                    },
+                };
+                const { data } = await axios.get(
+                    `${remote_config.BACKEND_URL}/api/orders/get-all-order`,
+                    config
+                );
+
+                console.log("data: ", data)
+
+                setAllOrder(data);
+            } catch (error) {
+                console.log('Error fetching all orders:', error);
+            }
+        };
+
+        fetchAllOrder();
+    }, [userInfo]);
 
   
 
     return (
-        <>
+ 
+        <Container>
             <Row>
                 <Col>
-                <div className='LoginForm-container'>
-                <div className='LoginForm'>
-                    <h2>User Profile</h2>
+                    <div className={styles.profileFormContainer}>
+                        <div className={styles.profileForm}>
+                            <h2 className={styles.form_header_text}>User Profile</h2>
 
-                    <div className='input-container'>
-                        <label>Name</label><br/>
-                        <input value={name} 
-                            placeholder='Enter your name'
-                            onChange={(event) => setName(event.target.value)}
-                        />
-                    </div>
+                            <div className='input-container'>
+                                <Input
+                                    label='Name'
+                                    value={name} 
+                                    placeholder='Enter your name'
+                                    onChange={(event) => setName(event.target.value)}
+                                />
+                            
+                            </div>
 
-                    <div className='input-container'>
-                        <label>Email Address</label><br/>
-                        <input value={email} 
-                            placeholder='Enter your email address'
-                            onChange={(event) => setEmail(event.target.value)}
-                        />
-                    </div>
+                            <div className='input-container'>  
+                                <Input
+                                    label='Email Address'
+                                    value={email}
+                                    placeholder='Enter your email address'
+                                    onChange={(event) => setEmail(event.target.value)}
+                                />
+                            </div>
 
-                    <div className='input-container'>
-                        <label>Password</label><br/>
-                        <input value={password} 
-                            type='password' 
-                            placeholder='Enter your email password'
-                            onChange={(event) => setPassword(event.target.value)}
-                        />
-                    </div>
+                            <div className='input-container'>
+                                <Input
+                                    label='Password'
+                                    value={password} 
+                                    type='password' 
+                                    placeholder='Enter your email password'
+                                    onChange={(event) => setPassword(event.target.value)}
+                                />
+                            </div>
 
-                    <div className='input-container'>
-                        <label>ConfirmPassword</label><br/>
-                        <input value={confirmPassword} 
-                            type='password' 
-                            placeholder='enter password again'
-                            onChange={(event) => setConfirmPassword(event.target.value)}
-                        />
-                    </div>
+                            <div className='input-container'>
+                                <Input
+                                    label='Confirm Password'
+                                    value={confirmPassword} 
+                                    type='password' 
+                                    placeholder='enter password again'
+                                    onChange={(event) => setConfirmPassword(event.target.value)}
+                                />
+                            </div>
 
-                    <div className='input-container'>
-                        <button type='submit' onClick={submitHandler}>UPDATE</button>
+                            <div className='input-container'>
+                                <button className={styles.submit_button} type='submit' onClick={submitHandler}>UPDATE</button>
+                            </div>
+                        
+                        </div>
+                
                     </div>
-                   
-                </div>
-                <ToastContainer/>
-               
-            </div>
                 </Col>
 
-                <Col md = {6}>
-                    <h1>My Order</h1>
-                </Col>
+                <Col md={6}>
+                    <div className={styles.order_card_container}>
+                        <div className={styles.order_list}>
+                            <h2>My Order</h2>
+
+                            {
+                                allOrder && allOrder.map((order) => (
+                                    <NavLink to={`/order/${order._id}`}>
+                                    <Row className={styles.order_list_row}>
+                                        <Col md={2}>
+                                            <img src={order.orderItems[0].product.image} alt='image' className={styles.order_list_image}></img>
+                                        </Col>
+                                        <Col md={4}>{order.orderItems[0].name} {order.orderItems.length > 1 && <span>and {order.orderItems.length -1} more items </span>}</Col>
+                                        <Col md={2}>{order.totalPrice}</Col>
+                                        <Col>{order.isDelivered ? 'Delivered' : 'On the way'}</Col>
+                                    </Row>
+                                    </NavLink>
+                                ))
+                            }
+
+
+
+                        </div>
+                        </div>
+                    </Col>
             </Row>
-           
-        
-        </>
+
+        </Container>
 
     )
 }
