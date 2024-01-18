871350570482522fcc8e04d240584f8e7cc2f5bf
mdtalha109 <mdtalha1041@gmail.com>
2024-01-19 00:16:42 +0530

    fix: cart clearing on successfull order

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -2,6 +2,7 @@ import axios from "axios"
 import { remote_config } from "../config/remoteURL"
 
 import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_LIST_FAIL, ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS } from "../constants/orderContants"
+import { CART_CLEAR_ITEMS } from "../constants/cartConstants"
 import { logout } from "./userActions"
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -27,11 +28,11 @@ export const createOrder = (order) => async (dispatch, getState) => {
         type: ORDER_CREATE_SUCCESS,
         payload: data,
       })
-    //   dispatch({
-    //     type: CART_CLEAR_ITEMS,
-    //     payload: data,
-    //   })
-    //   localStorage.removeItem('cart  Items')
+      dispatch({
+        type: CART_CLEAR_ITEMS,
+        // payload: data,
+      })
+      localStorage.removeItem('cartItems')
     } catch (error) {
       const message =
         error.response && error.response.data.message
