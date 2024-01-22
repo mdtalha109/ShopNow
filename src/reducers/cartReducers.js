63fda5b288483cee6f816921d13aba0195f04917
mdtalha109 <mdtalha1041@gmail.com>
2024-01-22 23:12:18 +0530

    fix: clear cart on successfull order

--- src/reducers/cartReducers.js
+++ src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_CLEAR_ITEMS, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants";
 
 export const cartReducer= (state= {cartItems:[], shippingAddress:[]}, action) =>{
      switch (action.type) {
@@ -29,6 +29,11 @@ export const cartReducer= (state= {cartItems:[], shippingAddress:[]}, action) =>
             return{
                 ...state,
                 shippingAddress: action.payload
+            }
+
+        case CART_CLEAR_ITEMS: 
+            return {
+                cartItems:[], shippingAddress:[]
             }
      
          default:
