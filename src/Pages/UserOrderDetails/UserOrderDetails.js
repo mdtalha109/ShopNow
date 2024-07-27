911fcf2ee84e26006cf96cc0e7cfa3fff8159f16
mdtalha109 <mdtalha1041@gmail.com>
2024-07-27 23:55:53 +0530

    feat: added order details page

--- src/Pages/UserOrderDetails/UserOrderDetails.js
+++ src/Pages/UserOrderDetails/UserOrderDetails.js
@@ -3,9 +3,14 @@ import { useEffect, useRef, useState } from "react";
 import { Container } from "react-bootstrap"
 import { useParams } from "react-router-dom";
 import ReactToPrint from "react-to-print";
-import Card from "../../components/Card/Card";
+
 import { remote_config } from "../../config/remoteURL";
-import './userOrderDetails.css'
+
+import Card from "../../components/ui/Card";
+
+import styles from "./index.module.css"
+import Table from "../../components/ui/Table";
+import Button from "../../components/ui/Button";
 
 
 
@@ -25,80 +30,77 @@ const UserOrderDetails = () => {
 
     return (
         <>
-        <Container className="orderDetails_container" ref= {ref}>
-            <h1>Order Details: #{id}</h1>
-            <br/>
-            {OrderDetails && 
-                <div>
-                    <Card style={{ fontSize:"1.2rem" }}>
-                        <div >
-                        <h3>Cusotmer Details:</h3>
-                        <div>Name: {OrderDetails.shippingAddress.address}</div>
-                        <div>Email Id: {OrderDetails.shippingAddress.city}</div>
-                        </div>
+            <Container className={styles.order_details_container} ref={ref}>
+                {OrderDetails &&
 
-                        <hr/>
+                    <Card >
+                        <Card.Header style={{ backgroundColor: "rgb(228, 228, 231", display: "flex" }}>
+                            <div >
+                                <div>
+                                    <h2 style={{ fontWeight: "bold", textTransform: "lowercase" }}>Order #{id}</h2>
+                                    <p>Placed on June 15, 2023</p>
+                                </div>
+                            </div>
+                        </Card.Header>
 
-                        <div className="orderDetails_container-address_details">
-                        <h3>Shipping Address:</h3>
-                        <div>Address: {OrderDetails.shippingAddress.address}</div>
-                        <div>City: {OrderDetails.shippingAddress.city}</div>
-                        <div>Postal Code: {OrderDetails.shippingAddress.postalCode}</div>
-                        <div>Country: {OrderDetails.shippingAddress.country}</div>
-                        </div>
-                    
-                        <hr/>
 
-                    
-                        <div>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>Product Name</th>
-                                        <th>Quantity</th>
-                                        <th>Price</th>
-                                    </tr>
-                                </thead>
-                                
-                                {OrderDetails.orderItems.map((item) =>                     
-                                    <tr>
-                                        <td>{item.name}</td>
-                                        <td>{item.qty}</td>
-                                        <td>{item.price}</td>
-                                    </tr>
-                                )}
-                            </table>
+                        <Card.Content>
+                            <div className="orderDetails_container-address_details">
+                                <h2 style={{ fontWeight: "bold", textTransform: "lowercase" }}>Shipping Address</h2>
+                                <div>Address: {OrderDetails.shippingAddress.address}</div>
+                                <div>City: {OrderDetails.shippingAddress.city}</div>
+                                <div>Postal Code: {OrderDetails.shippingAddress.postalCode}</div>
+                                <div>Country: {OrderDetails.shippingAddress.country}</div>
+                            </div>
+                        </Card.Content>
 
-                            <hr/>
+                        <Card.Content>
 
-                            <table className="total_details">                
-                                    <tr>
-                                        <td></td>
-                                        <td> Total </td>
-                                        <td>{OrderDetails.totalPrice}</td>
-                                    </tr>
-                                
-                            </table>
-                        </div>
-                    </Card> 
-                </div> 
-            }
-        </Container>
+                            <h2 style={{ fontWeight: "bold", textTransform: "lowercase" }}>Order Summary</h2>
 
-        {
-            OrderDetails &&  
+                            <Table>
+                                <Table.Head>
+                                    <Table.Row>
+                                        <Table.Column>Product Name</Table.Column>
+                                        <Table.Column>Quantity</Table.Column>
+                                        <Table.Column>Price</Table.Column>
+                                    </Table.Row>
+                                </Table.Head>
+
+                                <Table.Body>
+                                    {OrderDetails.orderItems.map((item) =>
+                                        <Table.Row>
+                                            <Table.Cell>{item.name}</Table.Cell>
+                                            <Table.Cell>{item.qty}</Table.Cell>
+                                            <Table.Cell>{item.price}</Table.Cell>
+                                        </Table.Row>
+                                    )}
+
+                                    <Table.Row>
+                                        <Table.Cell></Table.Cell>
+                                        <Table.Cell> Total </Table.Cell>
+                                        <Table.Cell>{OrderDetails.totalPrice}</Table.Cell>
+                                    </Table.Row>
+                                </Table.Body>
+                            </Table>
+                        </Card.Content>
+                    </Card>
+
+                }
+            </Container>
+
+            {
+                OrderDetails &&
                 <Container className="mt-4">
                     <ReactToPrint
                         bodyClass="print-agreement"
                         content={() => ref.current}
                         trigger={() => (
-                            <button className="download_button">Download Reciept</button>
-                         )}
+                            <Button className="download_button">Download Reciept</Button>
+                        )}
                     />
-
-
                 </Container>
-        }
+            }
         </>
     )
 }
