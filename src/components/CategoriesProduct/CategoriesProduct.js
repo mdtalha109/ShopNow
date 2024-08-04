dda9ec383d6cf9705971f5a0da184b89acb5bfe2
mdtalha109 <mdtalha1041@gmail.com>
2024-08-04 22:17:53 +0530

    refactor: moved style to its module scope

--- src/components/CategoriesProduct/CategoriesProduct.js
+++ src/components/CategoriesProduct/CategoriesProduct.js
@@ -2,44 +2,36 @@ import { Container } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { remote_config } from '../../config/remoteURL';
-
 import Product from '../Product';
 import ProductSkeletonLoader from '../SkeletonLoader/productSkeletonLoader';
 
-import './CategoriesProduct.css'
+import styles from './index.module.css'
 
-const CategoriesProduct = ({categoryName, Limit, heading, style}) => {
-
-    const CategoriesProductStyle= {
-        ...style
-    }
+const CategoriesProduct = ({ categoryName, Limit, heading, style }) => {
 
     const [product, setProduct] = useState([])
     const [loading, setLoading] = useState([])
 
-   
-        useEffect(async()=> {
-            const {data} = await axios.get(`${remote_config.BACKEND_URL}/api/products/category/${categoryName}?Limit=${Limit}`);
-            setProduct(data)
-            setLoading(false)
-        }, []);
-    
-   
-  return (
-      <>
-      <div style={CategoriesProductStyle}>
-      <h2 className='categories-title'>{heading}</h2>
-      
-        {<div className="container-product_card">
-            {loading ? <div style={{width:"100%", display: "flex", justifyContent:"center"}}><ProductSkeletonLoader/></div> : ""}
-            {product && product.map(product => <Product product = {product}/>)}
-            
-            </div>  
-        }
-        </div>
-    
-      </>
-  )
+
+    useEffect(async () => {
+        const { data } = await axios.get(`${remote_config.BACKEND_URL}/api/products/category/${categoryName}?Limit=${Limit}`);
+        setProduct(data)
+        setLoading(false)
+    }, []);
+
+
+    return (
+        <>
+            <div className={styles.category_product_container}>
+                <h1 className={styles.categories_title}>{heading}</h1>
+
+                <div className={styles.container_product_card}>
+                    {loading ? <div><ProductSkeletonLoader /></div> : ""}
+                    {product && product.map(product => <Product product={product} />)}
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default CategoriesProduct
