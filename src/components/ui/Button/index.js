3f6624e24aa4b5fe34c435ddb5a713910308c285
mdtalha109 <mdtalha1041@gmail.com>
2024-08-04 19:19:53 +0530

    style: full width button control

--- src/components/ui/Button/index.js
+++ src/components/ui/Button/index.js
@@ -5,9 +5,11 @@ import styles from './index.module.css'
 
 const Button = ({ children, onClick, className, style, ...rest }) => {
 
+    const isFullWidth = rest.isFullWidth || false
+    
     return (
         <button
-            className={`${styles.btn} ${className}`}
+            className={`${styles.btn} ${isFullWidth && styles.full_width_btn}`}
             style={style}
             onClick={onClick}
             {...rest}
