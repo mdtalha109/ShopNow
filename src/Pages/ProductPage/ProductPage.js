c24a0014b14be21d7880856a70e161db37944e47
mdtalha109 <mdtalha1041@gmail.com>
2024-08-11 21:58:58 +0530

    refactor: moved styling to css module

--- src/Pages/ProductPage/ProductPage.js
+++ src/Pages/ProductPage/ProductPage.js
@@ -1,16 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 import axios from 'axios';
 import Product from '../../components/Product';
-import { ListProducts } from '../../actions/productAction';
-import { useDispatch, useSelector } from 'react-redux';
-import CategoriesProduct from '../../components/CategoriesProduct/CategoriesProduct'
-import Categories from '../../components/Categories/Categories';
+import { useDispatch } from 'react-redux';
 import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
 import ProductNotFound from '../../components/ProductNotFound/ProductNotFound.js'
 import { remote_config } from '../../config/remoteURL';
@@ -38,26 +30,27 @@ const ProductPage = () => {
     function change(e){
         setSort(e.target.value)
     }
+
+    if(loading) return <LoadingSpinner/>
+    if(!loading && !product?.length) return <ProductNotFound/>
     
     return (
         <>
-            <span style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-                <div style={{marginTop: "20px", display:"flex", justifyContent:"space-between",alignItems:"center", width: "90%"}}>
-                <h2>{(product && product.length > 0)? categoryName:''}</h2>
-                    <select onChange={change} style={{width: "200px", height: "40px"}}>
+            <div className={styles.product_page_header_container}>
+                <div className={styles.product_page_header}>
+                    <h2>{(product && product.length > 0)? categoryName:''}</h2>
+                    <select onChange={change} className={styles.price_select}>
                         <option value="1">Low to High</option>
                         <option value="-1">High to low</option>
                     </select>
                 </div>
-            </span>
-            
+            </div>
           
-                <div className={styles.product_list}>
-                    {loading && <LoadingSpinner/>}
-                    {(product && product.length > 0)? product.map(product => (
-                       <Product product = {product} isLoading= {loading}/>  
-                    )): !loading && <ProductNotFound/>}
-                </div>
+            <div className={styles.product_list}>
+                {(product && product.length > 0)? product.map(product => (
+                    <Product product = {product} isLoading= {loading}/>  
+                )): null}
+            </div>
             
         </>
     )
